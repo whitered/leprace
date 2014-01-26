42ea66613f875f968c46b3ba5caa01d2f979d17a
Dmitry Zhelnin <dmitry.zhelnin@gmail.com>
2014-01-27 03:54:27 +0400

    show preview with replacements

--- leprace.js
+++ leprace.js
@@ -8,39 +8,66 @@
 
 (function($){
 
-  addWord = function(index, data){
-    console.log($('td.fow_word', data).text(), $('td.fow_to_word', data).text());
+  var addWord = function(index, data){
+    var from = $("td.fow_word", data).text();
+    var to = $("td.fow_to_word", data).text();
+    replacements.push([from, to]);
   }
 
 
-  loadReplacementsPage = function(page){
+  var loadReplacementsPage = function(page){
     var url = "http://leprosorium.ru/fraud/textomate/" + page;
-    console.log('loading', url);
     $.get(url, parseReplacements);
   }
 
 
-  parseReplacements = function(data){
-    words = $('table.fraud_one_word', data);
+  var parseReplacements = function(data){
+    words = $("table.fraud_one_word", data);
     words.each(addWord);
+
+    updatePreview();
 
     scripts = $("script:not([src])", data);
     scripts.each(function(index, data){
-      console.log('check script');
       md = data.innerText.match(/pag = new Paginator\('paginator', (\d+), \d+, (\d+)/);
       if(md){
-        console.log('pager found');
         pagesTotal = Number(md[1]);
         currentPage = Number(md[2]);
-        console.log(pagesTotal, currentPage);
         if(currentPage < pagesTotal) loadReplacementsPage(currentPage + 1);
         return;
       }
     });
   }
 
+
+  var replace = function(text){
+    for(var i = replacements.length - 1; i >= 0; i--){
+      r = replacements[i];
+      text = text.replace(r[0], r[1]);
+    };
+    return text;
+  }
+
+  var updatePreview = function(){
+    preview.html(replace(textarea.val()));
+  }
+
+  var preview = $("<div></div>");
+  $("#reply_form").prepend(preview);
+
+  var textarea = $("#comment_textarea");
+  textarea.on('input propertychange', updatePreview);
+
+  var replacements = [];
+
+  diprf = displayInPlaceReplyForm;
+  displayInPlaceReplyForm = function(replyLink, userLink){
+    diprf(replyLink, userLink);
+    updatePreview();
+    return false;
+  }
+
+
   loadReplacementsPage(1);
-
-
 })(window.jQ);
 
